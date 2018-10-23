41a61784cf6374fe1077645d94a8f047b3d6ee13
Colin Galindo <colin.galindo@egov.com>
2018-10-22 20:51:19 -0500

    Updates

---
+++ precache-manifest.471fd3a1f13e996dfe3b073913a4aecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dice-roller/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b82219d53e73cebcad0d",
+    "url": "/dice-roller/static/js/runtime~main.b82219d5.js"
+  },
+  {
+    "revision": "8788dd0b63889f8a4846",
+    "url": "/dice-roller/static/js/main.8788dd0b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/dice-roller/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "8788dd0b63889f8a4846",
+    "url": "/dice-roller/static/css/main.5258f95c.chunk.css"
+  },
+  {
+    "revision": "1b2b4ccd947904fd27f0265d30a3f4a1",
+    "url": "/dice-roller/index.html"
+  }
+];
