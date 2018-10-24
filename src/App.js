2c0ff3037cf50fad0852b9403e259cca8ce14848
Colin Galindo <galindo.colin@gmail.com>
2018-10-24 17:38:53 -0500

    Feature: Number of Dice Field (#7)

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 class App extends Component {
   state = {
     numSides: 0,
+    numDice: 1,
   };
 
   handleChange = event => {
+    const { name, value } = event.target;
+
     this.setState({
-      numSides: event.target.value,
+      [name]: value,
     });
   };
 
   render() {
     const numSides = this.state.numSides;
+    const numDice = this.state.numDice;
 
     return (
       <div className="App">
-        <select onChange={this.handleChange} value={numSides}>
+        <select name="numSides" onChange={this.handleChange} value={numSides}>
           <option value={0}>Select a Dice</option>
           <option value={4}>D4</option>
           <option value={6}>D6</option>
@@ -26,6 +30,18 @@ class App extends Component {
           <option value={12}>D12</option>
           <option value={20}>D20</option>
         </select>
+        <br />
+        <label htmlFor="numDice">
+          {'Please Enter number of Dice you would like to roll:'}
+        </label>
+        <input
+          id="numDice"
+          name="numDice"
+          min="1"
+          onChange={this.handleChange}
+          type="number"
+          value={numDice}
+        />
       </div>
     );
   }
